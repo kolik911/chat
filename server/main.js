d583bfe66d5a31752b68c42bbad6ef3c9e4a99b7
Mykola Kachmaruk <kolia.kachmaruk@gmail.com>
2017-12-29 20:20:33 +0200

    fix

--- server/main.js
+++ server/main.js
@@ -4,11 +4,7 @@ import '../imports/api/methods/methods.js';
 import { Rooms } from '../imports/api/rooms';
 
 Meteor.startup(() => {
-<<<<<<< HEAD
   
-=======
- 
->>>>>>> ca6fb380f204d0be1ef9443c9677d1c5486f0fcb
   if (Rooms.find().count() === 0) {
     Meteor.call('addRoom', 'Sambir');
     Meteor.call('addRoom', 'Kyiv');
